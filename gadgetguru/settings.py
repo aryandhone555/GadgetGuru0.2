71ee2b6a8beb78823931a28ff79e940450a37e9e
Aryan Dhone <er.aryandhone@gmail.com>
2025-06-07 19:11:10 +0530

    Update settings.py

--- gadgetguru/settings.py
+++ gadgetguru/settings.py
@@ -117,10 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # <-- collectstatic collects here
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
